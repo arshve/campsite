01c1e828bad1e5c1290270f289a651abb6f337b5
Farid Evan <faridevan97@gmail.com>
2018-11-26 16:13:19 +0000

    add db

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ var commentRoutes    = require("./routes/comments"),
 // Seed the Database
 // seedDB();
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+mongoose.connect("mongodb://farvan:farvan97@ds117334.mlab.com:17334/farvan", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+// mongodb://farvan:farvan97@ds117334.mlab.com:17334/farvan
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
